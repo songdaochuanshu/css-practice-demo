aec7febf2b30d6c1ca2df09a7649739d6a56ce14
songdaochuanshu <songdaochuanshu@gmail.com>
2021-12-21 14:30:21 +0800

    add config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 松岛川树
+ * @Date: 2021-12-21 14:28:27
+ * @LastEditors: 松岛川树
+ * @LastEditTime: 2021-12-21 14:28:28
+ * @FilePath: \CSS3\vite.config.js
+ */
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/css-practice-demo/",
+});
